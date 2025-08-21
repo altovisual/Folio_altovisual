4e1c5bb6eb18271673cbd84a487f43e1a3de9691
altovisual <manuelalejandromendozaalvarado@gmail.com>
2025-08-21 18:37:26 -0300

    fix: Correct unterminated string literal in altovisual-portrait-grid.tsx

--- components/altovisual-portrait-grid.tsx
+++ components/altovisual-portrait-grid.tsx
@@ -116,7 +116,7 @@ export function AltovisualPortraitGrid({
         "7",
         "1",
         "/",
-        "\\
+        "\\",
         "|",
         "(",
         ")",
@@ -134,7 +134,7 @@ export function AltovisualPortraitGrid({
         "!",
         ">",
         "<",
-        '"',
+        "\"",
         "^",
         "=",
         ",",
